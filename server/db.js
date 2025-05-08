0e3f27bd2c262391395c1ca43bbff27cd197884e
imane <i07174000@gmail.com>
2025-05-08 15:31:04 +0100

    railway

--- server/db.js
+++ server/db.js
@@ -1,14 +1,17 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: "mysql",
   logging: false,
   dialectOptions: {
-    connectTimeout: 10000, // زيادة المهلة إلى 10 ثواني
+    connectTimeout: 10000,
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
   },
 });
+
 
 export default sequelize;
